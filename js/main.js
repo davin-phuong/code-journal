7064b47e50791d155b771e1718938e6dfcf73a66
Davin Phuong <davinphuong@yahoo.com>
2022-02-14 21:33:53 -0800

    Listen for input events on photoUrl. #1.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.querySelector('#photo');
+
+var $img = document.querySelector('img');
+
+$photoUrl.addEventListener('input', function (event) {
+  $img.setAttribute('src', $photoUrl.value);
+});
